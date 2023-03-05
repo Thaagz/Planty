815e8387ac7e34e0f57894b564e068b58d27554c
Thaagz <coutable.valet.g@gmail.com>
2023-03-05 19:54:33 +0100

    mise en place d'un CPT et d'un ACF

--- wp-content/themes/oceanwp-child/functions.php
+++ wp-content/themes/oceanwp-child/functions.php
@@ -28,3 +28,56 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 // require '/shortcode/banner.php';
 require __DIR__. '/shortcode/banner.php';
 
+// CPT
+
+/*
+* On utilise une fonction pour créer notre custom post type 'Commandes'
+*/
+
+function order_custom_post_type() {
+
+	// On rentre les différentes dénominations de notre custom post type qui seront affichées dans l'administration
+	$labels = array(
+		// Le nom au pluriel
+		'name'                => _x( 'Commandes', 'Post Type General Name'),
+		// Le nom au singulier
+		'singular_name'       => _x( 'Commande', 'Post Type Singular Name'),
+		// Le libellé affiché dans le menu
+		'menu_name'           => __( 'Commandes'),
+		// Les différents libellés de l'administration
+		'all_items'           => __( 'Toutes les Commandes'),
+		'view_item'           => __( 'Voir les Commandes'),
+		'add_new_item'        => __( 'Ajouter une nouvelle Commande'),
+		'add_new'             => __( 'Ajouter'),
+		'edit_item'           => __( 'Editer la Commandes'),
+		'update_item'         => __( 'Modifier la Commandes'),
+		'search_items'        => __( 'Rechercher une Commande'),
+		'not_found'           => __( 'Non trouvée'),
+		'not_found_in_trash'  => __( 'Non trouvée dans la corbeille'),
+	);
+	
+	// On peut définir ici d'autres options pour notre custom post type
+	
+	$args = array(
+		'label'               => __( 'Commandes'),
+		'description'         => __( 'Liste des commandes'),
+		'labels'              => $labels,
+		// On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
+		'supports'            => array( 'title', 'custom-fields', ),
+		/* 
+		* Différentes options supplémentaires
+		*/
+		'show_in_rest'        => true,
+		'hierarchical'        => false,
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			  => array( 'slug' => 'orders'),
+
+	);
+	
+	// On enregistre notre custom post type qu'on nomme ici "serietv" et ses arguments
+	register_post_type( 'orders', $args );
+
+}
+
+add_action( 'init', 'order_custom_post_type', 0 );
